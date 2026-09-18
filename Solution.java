0914290b56126ad31f66d82620be463440c0ea14
Babloo Kumar Kushwaha <mauryababloo210@gmail.com>
2026-09-18 10:32:47 +0530

    Linkedlist cycle

--- Solution.java
+++ Solution.java
@@ -1,14 +1,47 @@
-class Solution {
-    public int removeElement(int[] nums, int val) {
-        int i = 0;
+/**
+ * Definition for singly-linked list.
+ */
+public class Solution {
+    public static class ListNode {
+        private final int val;
+        ListNode next;
 
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != val) {
-                nums[i] = nums[j];
-                i++;
+        public ListNode(int x) {
+            val = x;
+            next = null;
+        }
+
+        public int getVal() {
+            return val;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
             }
         }
 
-        return i;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = head;
+
+        System.out.println(new Solution().hasCycle(head));
     }
 }
